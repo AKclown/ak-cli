3e40f4f82d51ff462730a8df5e9595b1255e0c70
AKclown <13413629530@139.com>
2022-01-14 10:49:11 +0800

    添加npminstall测试demo

---
+++ core/cli/lib/npminstall-test.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+const npminstall = require('npminstall');
+const path = require('path');
+const userHome = require('userhome')();   // C:\Users\ak
+
+npminstall({
+    root: path.resolve(userHome, '.ak-cli'),
+    storeDir: path.resolve(userHome, '.ak-cli', 'node_modules'),
+    registry: 'https://registry.npmjs.org',
+    pkgs: [
+        { name: 'foo', version: '~1.0.0' }
+    ]
+})
