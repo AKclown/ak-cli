89a1509910c336653f0044232f5894c8a5cc8431
AKclown <13413629530@139.com>
2022-02-08 11:26:37 +0800

    完善获取模板信息

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,9 +1,9 @@
 const Command = require('@ak-clown/command');
 const log = require('@ak-clown/log');
-const path = require('path');
 const fs = require('fs');
 const inquirer = require('inquirer');
-const fse = require('fs-extra')
+const fse = require('fs-extra');
+const semver = require('semver')
 
 // 项目/组件
 const TYPE_PROJECT = 'project';
@@ -17,17 +17,34 @@ class InitCommand extends Command {
         log.verbose('force', this.force);
     }
 
-    exec() {
+    async exec() {
         /**
          * 1.准备阶段
          * 2.下载模板
-         * 3. 安装模板
+         * 3.安装模板
          */
         try {
-            this.prepare();
+            // 1.准备阶段
+            const projectInfo = await this.prepare();
+            if (projectInfo) {
+                //  2.下载模板
+                log.verbose('projectInfo', projectInfo);
+                this.downloadTemplate();
+                // 3. 安装模板
+            }
         } catch (error) {
             log.error(error.message);
         }
+    }
+
+    downloadTemplate() {
+        /**
+         * 1.通过项目模板API获取项目模板信息
+         * 2.通过egg.js搭建一套后台系统
+         * 3.通过npm存储项目模板
+         * 4.将项目模板储存到mongodb数据库中
+         * 5.通过egg.js获取mongodb中的数据并且通过API返回
+         */
     }
 
     async prepare() {
@@ -43,14 +60,13 @@ class InitCommand extends Command {
             let ifContinue = false;
             // $ 强制更新 - 不给予提示
             if (!this.force) {
-                ifContinue = await inquirer
+                ifContinue = (await inquirer
                     .prompt({
                         type: 'confirm',
                         name: 'ifContinue',
                         default: false,
                         message: '当前文件不为空，是否继续创建项目?'
-                    }).ifContinue;
-
+                    })).ifContinue;
                 if (!ifContinue) {
                     return;
                 }
@@ -68,24 +84,24 @@ class InitCommand extends Command {
                     // $ 清空当前目录
                     fse.emptyDirSync(localPath);
                 }
-
             }
         }
 
-        return this.getProjectInfo();
+        return await this.getProjectInfo();
     }
 
+    // 判断当前目录是否为空
     isCwdEmpty(localPath) {
         let fileList = fs.readdirSync(localPath);
+        // 忽略掉.开头文件 以及 node_modules目录
         fileList = fileList.filter(file => (
-            !file.startsWith('.') && !['node_module'].includes(file)
+            !file.startsWith('.') && !['node_modules'].includes(file)
         ))
         console.log('fileList: ', fileList);
         return !fileList || fileList.length <= 0;
     }
 
-    getProjectInfo() {
-        const projectInfo = {};
+    async getProjectInfo() {
         // 1. 选择创建项目或组件
         const { type } = await inquirer
             .prompt([{
@@ -103,7 +119,7 @@ class InitCommand extends Command {
             }]);
         log.verbose('type', type);
 
-        const o = await inquirer
+        const { projectName, projectVersion } = await inquirer
             .prompt([
                 {
                     type: 'input',
@@ -111,7 +127,20 @@ class InitCommand extends Command {
                     message: '请输入项目名称',
                     default: '',
                     validate: function (v) {
-                        return v;
+                        const done = this.async();
+                        /**
+                         * 1. 首字母必须为英文字符
+                         * 2. 尾字符必须为英文或数字，不能为字符
+                         * 3. 字符仅允许“-_”
+                         */
+                        setTimeout(function () {
+                            if (!/^[a-zA-Z]+([-][a-zA-Z][a-zA-Z0-9]*|[_][a-zA-Z][a-zA-Z0-9]|[a-zA-Z0-9]*)*$/.test(v)) {
+                                done('请输入合法的项目名称');
+                                return;
+                            }
+                            // Pass the return value in the done callback
+                            done(null, true);
+                        }, 0);
                     },
                     filter: function (v) {
                         return v;
@@ -120,21 +149,28 @@ class InitCommand extends Command {
                     type: 'input',
                     name: 'projectVersion',
                     message: '请输入项目版本号',
-                    default: '',
+                    default: '1.0.0',
                     validate: function (v) {
-                        return v === 'string';
+                        return !!semver.valid(v);
                     },
                     filter: function (v) {
-                        return v;
+                        if (!!semver.valid(v)) {
+                            // 能够将V1.0.0转为 1.0.0
+                            return semver.valid(v);
+                        } else {
+                            return v
+                        }
                     }
                 }
-            ])
+            ]);
         // 2. 获取项目的基本信息
         if (type === TYPE_PROJECT) {
 
         } else if (type === TYPE_COMPONENT) {
 
         }
+
+        const projectInfo = { type, projectName, projectVersion };
         // 返回项目的基本信息
         return projectInfo
     }
